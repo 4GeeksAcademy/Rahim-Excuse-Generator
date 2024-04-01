65ebfc25530c138864988279a4e4b15eac692d93
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-01 00:47:42 +0000

    random math floor added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the cat", "my father", "the bird", "my neighbor"];
+  let action = ["fought", "watch", "fly", "ate", "broke"];
+  let what = ["the phone", "the glass", "the fence", "ninja", "thief"];
+  let when = ["after lunch", "bedtime", "before the class", "while playing"];
+
+  let excuseGenerate =
+    who[rand(who.length)] +
+    " " +
+    what[rand(what.length)] +
+    " " +
+    where[rand(where.length)];
+  let excuseSpan = document.querySelector("#excuse");
+  excuseSpan.innerHTML = excuseGenerate;
 };
+function rand(length) {
+  let number = Math.floor(Math.random() * length);
+  return number;
+}
