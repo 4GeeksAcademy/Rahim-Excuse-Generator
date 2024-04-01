7fe18e76818dd69a6036e28215e5548f660528d5
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-01 01:12:00 +0000

    syntax error fixed

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let who = ["the cat", "my father", "the bird", "my neighbor"];
-  let action = ["fought", "watch", "fly", "ate", "broke"];
-  let what = ["the phone", "the glass", "the fence", "ninja", "thief"];
-  let when = ["after lunch", "bedtime", "before the class", "while playing"];
+let who = ["the cat", "my father", "the bird", "my neighbor"];
+let action = ["fought", "watch", "fly", "ate", "broke"];
+let what = ["the phone", "the glass", "the fence", "ninja", "thief"];
+let when = ["after lunch", "bedtime", "before the class", "while playing"];
+function generateExcuse() {
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-  let excuseGenerate =
-    who[rand(who.length)] +
-    " " +
-    what[rand(what.length)] +
-    " " +
-    where[rand(where.length)];
-  let excuseSpan = document.querySelector("#excuse");
-  excuseSpan.innerHTML = excuseGenerate;
-};
-function rand(length) {
-  let number = Math.floor(Math.random() * length);
-  return number;
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
 }
+window.onload = function() {
+  document.getElementById("excuse").textContent = generateExcuse();
+};
