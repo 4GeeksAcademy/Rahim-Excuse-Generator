2b0cb171fd3757da08852fe5217cd93a48158e3a
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-02 17:11:24 +0000

    script link added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+// excuses
 let who = ["the cat", "my father", "the bird", "my neighbor"];
 let action = ["fought", "watch", "fly", "ate", "broke"];
 let what = ["the phone", "the glass", "the fence", "ninja", "thief"];
@@ -12,7 +12,5 @@ function generateExcuse() {
   let randomAction = action[Math.floor(Math.random() * action.length)];
   let randomWhat = what[Math.floor(Math.random() * what.length)];
   let randomWhen = when[Math.floor(Math.random() * when.length)];
-
-  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
 }
-
+const randomExcuse = Math.floor(Math.random() * generateExcuse.length);
