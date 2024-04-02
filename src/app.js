d69ac6c6fa3d4046d45edea9ec5bd1ed37b1cbb3
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-02 17:48:29 +0000

    cancatination added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
-/* eslint-disable */
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-// excuses
+// excuses variables declared
 let who = ["the cat", "my father", "the bird", "my neighbor"];
 let action = ["fought", "watch", "fly", "ate", "broke"];
 let what = ["the phone", "the glass", "the fence", "ninja", "thief"];
 let when = ["after lunch", "bedtime", "before the class", "while playing"];
+// function declared
 function generateExcuse() {
   let randomWho = who[Math.floor(Math.random() * who.length)];
   let randomAction = action[Math.floor(Math.random() * action.length)];
   let randomWhat = what[Math.floor(Math.random() * what.length)];
   let randomWhen = when[Math.floor(Math.random() * when.length)];
+  //concatinating all the variables
+  let excuses =
+    randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+  console.log(excuses);
+  return excuses;
 }
-const randomExcuse = Math.floor(Math.random() * generateExcuse.length);
